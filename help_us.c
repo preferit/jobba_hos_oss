467b42fa79c7d96c24df065c970b9810e25959a3
Alexander Löfqvist <alexander.lofqvist@gmail.com>
2022-05-05 11:30:19 +0200

    Create help_us.c

---
+++ help_us.c
@@ -0,0 +1,13 @@
+/******************************************************************************
+
+Welcome Friend!
+It seems like the implementation of strcmp in the c standard library has
+been lost in my universe. 
+Please help us to implement it.
+
+*******************************************************************************/
+
+int main()
+{
+    return 0;
+}
